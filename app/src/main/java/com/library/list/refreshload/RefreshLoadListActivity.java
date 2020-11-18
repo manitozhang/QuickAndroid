ef1ec40a23f6d6a319ea0dd621e15c0af7a818aa
zhangpengfei <qq1271396448>
2020-11-18 14:13:28 +0800

    新增列表相关

---
+++ app/src/main/java/com/library/list/refreshload/RefreshLoadListActivity.java
@@ -0,0 +1,35 @@
+package com.library.list.refreshload;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.common.base.BaseActivity;
+import com.library.R;
+
+/**
+ * 刷新加载集合
+ */
+public class RefreshLoadListActivity extends BaseActivity {
+
+    @Override
+    public int getLayout() {
+        return R.layout.activity_refresh_load_list;
+    }
+
+    @Override
+    public void initViewIds() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData(@Nullable Bundle savedInstanceState) {
+
+    }
+}
